49acd744024fc05480ce113e1e7599f80e8ebecb
fisherjim1605 <fisherjim1605@gmail.com>
2020-11-22 12:51:11 +0000

    altered page formatting, images now move right when clicked

--- index.js
+++ index.js
@@ -1,74 +1,33 @@
-//Canvas scripts for canvas stored in the index.js as to not overcrowd index.html file
-var canvas = document.getElementById("gameCanvas");
-var context = canvas.getContext("2d");
+//Function scripts for images stored in the index.js as to not overcrowd index.html file
+let wolfImage = document.getElementById("wolf");
+wolfImage.style.position = "relative";
+wolfImage.style.left = "0px";
 
-var wolfImage = document.getElementById("wolf");
-var goatImage = document.getElementById("goat");
-var cabbageImage = document.getElementById("cabbage");
 
-context.drawImage(wolfImage, 10, 10);
-context.drawImage(goatImage, 10, 10);
-context.drawImage(cabbageImage, 10, 10);
+let goatImage = document.getElementById("goat");
+goatImage.style.position = "relative";
+goatImage.style.left = "0px";
+
+let cabbageImage = document.getElementById("cabbage");
+cabbageImage.style.position = "relative";
+cabbageImage.style.left = "0px";
+
 
 function getImage(image) {
     alert(image.alt);
+    moveImage(image.alt);
 }
 
-/*
-context.beginPath();
-context.rect(20, 40, 50, 50);
-context.fillStyle = "#FF0000";
-context.fill();
-context.closePath();
-
-let Boat = class Boat {
-    constructor() {
-        this.passenger = 'EMPTY';
-    }
-
-    set passenger(passenger) {
-        this._passenger = passenger;
-    }
-
-    get passenger() {
-        return this._passenger
-    }
-    get isEmpty() {
-        if (this._passenger == 'EMPTY') {
-            return true;
-        } else {
-            return false;
-        }
+function moveImage(imageName) {
+    //Take current image and its left offset, and add 20 pixels to move it right
+    if(imageName == "Wolf") {
+        wolfImage.style.left = (wolfImage.offsetLeft) + 20 + "px";
+    } else if(imageName == "Goat") {
+        goatImage.style.left = (goatImage.offsetLeft) + 20 + "px";
+    } else if (imageName == "Cabbage") {
+        cabbageImage.style.left = (cabbageImage.offsetLeft) + 20 + "px";
     }
 }
 
-let boat = new Boat();
-let passenger = window.prompt("Who is boarding the boat?");
-boat.passenger = passenger;
 
-alert(boat.passenger);
-alert(boat.isEmpty);
-
-
-function myKeyPress(e) {
-    e = e || window.event;
-
-    if (e.keyCode == '38') {
-        alert('UP');
-        // up arrow
-    }
-    else if (e.keyCode == '40') {
-        alert('DOWN');
-        // down arrow
-    }
-    else if (e.keyCode == '37') {
-        alert('LEFT');
-       // left arrow
-    }
-    else if (e.keyCode == '39') {
-        alert('RIGHT');
-       // right arrow
-    }
-}
-*/
 
